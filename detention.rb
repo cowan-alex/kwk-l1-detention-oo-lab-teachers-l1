e150067ee94134a2b4cba98adad607120266a0b3
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-06-22 19:16:37 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,61 @@
 #Code your detention class here
+class Detention 
+  
+  def initialize
+    @activity = "collective punishment"
+    @students = []
+  end
+  
+  def activity
+    return @activity
+  end
+  
+  def activity=(activity)
+    @activity = activity
+  end
+  
+  def location
+    return @location
+  end
+  
+  def location=(location)
+    @location = location
+  end
+  
+  def time
+    return @time
+  end
+  
+  def time=(time)
+    @time = time
+  end
+  
+  def grumpy_teacher
+    return @grumpy_teacher
+  end
+  
+  def grumpy_teacher=(grumpy_teacher)
+    @grumpy_teacher = grumpy_teacher
+  end
+  
+  def students
+    return @students
+  end
+  
+  def students=(students)
+    @students = students 
+  end
+  
+  def add_student
+    return @add_student
+  end
+  
+  def add_student(add_student)
+    @add_student = @students << add_student
+  end
+  
+  def remove_student(remove_student)
+    @students.delete(remove_student)
+  end
+  
+end
